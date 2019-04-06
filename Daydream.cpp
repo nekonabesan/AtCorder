dc58f007687a72e9bd58f0649f9e914092fc0acf
nekonabesan <sheepdogs_2@hotmail.com>
2019-04-06 14:02:45 +0900

    2019/04/06

--- Daydream.cpp
+++ Daydream.cpp
@@ -8,7 +8,6 @@ using namespace std;
 
 int main(){
   string s;
-  string t;
   string dream("dream");
   string dreamer("dreamer");
   string erase("erase");
@@ -20,53 +19,48 @@ int main(){
   int j = 0;
   int k = 0;
   int pos = 0;
-  int cnt = 0;
   bool flg = true;
   cin >> s;
-  cnt = s.size();
+  string t(s.size(), 0x30);
   for(i = 0; i < s.size(); i++){
     if(s[i] < 97 || 122 < s[i]){
       flg = false;
       break;
     }
   }
-  if(cnt < 1 || (int)pow(10.0, 5.0) < cnt || flg == false){
+  if(s.size() < 1 || (int)pow(10.0, 5.0) < s.size() || flg == false){
     return 1;
   }
-  // tを初期化する処理
-  for(i = 0; i < cnt; i++){
-    t[i] = 0x30;
-  }
   //
-  for(i = 0; i < cnt; i++){
+  for(i = 0; i < s.size(); i++){
     for(j = 0; j < 4; j++){
       switch(j){
         case 0:
           tmp = dream;
           break;
         case 1:
-          tmp = eraser;
+          tmp = erase;
           break;
         case 2:
-          tmp = dreamerase;
+          tmp = dreamer;
           break;
         case 3:
-          tmp = eraseerase;
+          tmp = eraser;
           break;
         default:
           tmp = "\0";
           break;
       }
-      for(k = 0; k < tmp.size(); k++){
-        if(s[i + k] == tmp[k]){
-          t[i + k] = tmp[k];
-          printf("%c\n",t[i + k]);
+
+      if(!tmp.empty() && (i + tmp.size()) <= s.size()){
+        for(k = 0; k < tmp.size(); k++){
+          if(s[i + k] == tmp[k]){
+            t[i + k] = tmp[k];
+          } else {
+            break;
+          }
         }
       }
-    }
-
-    if(s.size() <= t.size()){
-      break;
     }
   }
 
@@ -74,7 +68,7 @@ int main(){
     flg = false;
   }
 
-  for(i = 0; i < t.size(); i++){
+  for(i = 0; i < s.size(); i++){
     if(s[i] != t[i]){
       flg = false;
       break;
